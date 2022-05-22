ebb716fadac512fa8f502b26a803ccf82aa49624
HabiburRahmanAnik <habib264521@gmail.com>
2022-05-22 09:46:26 +0600

    complete parts section

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import { ToastContainer } from 'react-toastify';
 import Navbar from './components/Layout/Navbar';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Pages/Home/Home';
-
+import { QueryClientProvider, QueryClient } from 'react-query';
 import 'react-toastify/dist/ReactToastify.css';
+
+const client = new QueryClient();
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
+      <QueryClientProvider client={client}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </QueryClientProvider>
       <ToastContainer />
     </div>
   );
--- src/components/Pages/Home/Parts.js
+++ src/components/Pages/Home/Parts.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import axios from 'axios';
+import React from 'react';
+import { useQuery } from 'react-query';
+import PartsList from './PartsList';
 
 const Parts = () => {
-  return (
-    <div>Parts</div>
-  )
-}
+  const { data, isLoading } = useQuery('parts', () => axios.get('parts.json'));
 
-export default Parts
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className="py-20 px-3">
+      <h2 className="text-lg lg:text-3xl uppercase font-semibold text-primary text-center pb-5">
+        Our Parts
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 lg:max-w-7xl mx-auto">
+        {data?.data.map((part) => (
+          <PartsList key={part.id} part={part} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Parts;
