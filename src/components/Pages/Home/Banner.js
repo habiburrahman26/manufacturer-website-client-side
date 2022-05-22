81f88f2311bf346b70b1728b2c41b6a57c04fc73
HabiburRahmanAnik <habib264521@gmail.com>
2022-05-22 08:43:52 +0600

    add banner

--- src/components/Pages/Home/Banner.js
+++ src/components/Pages/Home/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
         <div class="hero-overlay bg-opacity-75"></div>
         <div class="hero-content text-center text-neutral-content">
           <div class="max-w-md">
-            <h1 class="mb-5 text-5xl font-bold">
+            <h1 class="mb-5 text-5xl font-bold" style={{lineHeight:'1.2'}}>
               Are you ready for your next business?
             </h1>
             <p class="mb-5 text-lg">
